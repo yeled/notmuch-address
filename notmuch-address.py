95c229f30031e5022d3441bd0b266a6b7858ca80
Charlie Allom <charlie@evilforbeginners.com>
2017-06-10 20:45:49 +0100

    working version that inserts and replaces on a static query

--- notmuch-address.py
+++ notmuch-address.py
@@ -1,5 +1,38 @@
 import sublime
+import sublime_plugin
+import subprocess
+
+class NotmuchaddressCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        text = None
+        args = ['/usr/local/bin/notmuch', 'address', 'email.com']
+
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, universal_newlines=True)
+        items = proc.stdout.readlines()
+
+        def callback(i):
+            if i == -1:
+                    return
+            item = items[i]
+            #self.view.run_command("insert", {"characters": item})
+            self.view.run_command("notmuchedit", {"text": item.rstrip('\n')})
+            print(item)
+            #self.view.insert(edit, self.view.sel()[0].begin(), item)
+
+        self.view.window().show_quick_panel(items, callback)
 
 
-ST3 = sublime.version() == '' or int(sublime.version()) > 3000
+class NotmucheditCommand(sublime_plugin.TextCommand):
 
+    def run(self, edit, text):
+        sel = self.view.sel()
+        selected = None
+        args = []
+        if len(sel) > 0:
+            selected = sel
+        for region in sel:
+            if region.size() == 0:
+                self.view.insert(edit, region.end(), text)
+            else:
+                self.view.replace(edit, region, text)
